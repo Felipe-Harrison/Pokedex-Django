ce55b63e3552f0ad0149febbed64272e5fcd5b94
Harrisu <76136248+Felipe-Harrison@users.noreply.github.com>
2022-03-09 20:08:49 -0300

    Delete Pokedex directory

--- Pokedex/pokemon/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import pokemon
-# Register your models here.
-
-@admin.register(pokemon)
-class PokemonAdmin(admin.ModelAdmin):
-    ordering = ('number','name')
-    list_display = ("number","name","type1","type2")
